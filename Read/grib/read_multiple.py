8c8265b740ee88070e7bff283f9df9528b560b7b
CAUI-IMDPune <92711156+CAUI-IMDPune@users.noreply.github.com>
2021-10-29 14:41:56 +0530

    Update read_multiple.py

--- Read/grib/read_multiple.py
+++ Read/grib/read_multiple.py
@@ -1,8 +1,6 @@
 import xarray as xr
 
 ds=xr.open_mfdataset('/mnt/e/Python_Scripts/Sample_Data/ERA5_Temperature_*.grib',engine='cfgrib',backend_kwargs={'indexpath':''})
-
-ds1=xr.open_mfdataset('/mnt/e/Python_Scripts/Sample_Data/ERA5_Temperature_*.grib',engine='cfgrib',backend_kwargs={'indexpath':''})
 print(ds)		###Print summary of the file
 #print(ds.t)		###Print summary of the variable
 #print(ds.latitude)
