8573fbd3e8c1608e2bd26acf1b6b1a2e61e3fdf9
CAUI-IMDPune <92711156+CAUI-IMDPune@users.noreply.github.com>
2021-11-10 10:42:49 +0530

    Update vector_overlay_basemap.py

--- Plotting/overlay/vector_overlay_basemap.py
+++ Plotting/overlay/vector_overlay_basemap.py
@@ -7,9 +7,8 @@ import datetime as dt
 from mpl_toolkits.basemap import Basemap
 
 ############ File to be read ####################
-############ IMD Gridded Rainfall data ##########
 
-file_name  = filename='/mnt/d/DATA/ERA5/Wind/ERA5_Wind_2019.nc'
+file_name  ='/mnt/d/DATA/ERA5/Wind/ERA5_Wind_2019.nc'
 
 ################# open file ######################
 
@@ -28,7 +27,7 @@ u10   = f.variables['u10'][:]
 v10   = f.variables['v10'][:]
 lats = f.variables['latitude'][:]
 lons = f.variables['longitude'][:]
-time = f.variables['time']		# In the file for the time dimension year has been set as 2010 in all year files
+time = f.variables['time']		
 
 #print(lons.min()," ,",lons.max())
 #print(lats)
