8041b05b17a966c378c1fcae4a6cce2a8035326d
Lekshmi S <94049317+lekshmis9403@users.noreply.github.com>
2021-12-14 19:55:53 +0530

    Update read_grib.py

--- Read/grib/read_grib.py
+++ Read/grib/read_grib.py
@@ -1,5 +1,11 @@
+#######PYTHON CODE FOR IMD PREPARED BY LEKSHMI S#########
+########## https://doi.org/10.5281/zenodo.5674826 ############
+
+#######READ A GRIB FILE###########################
+#########Import necessary modules##################
 import xarray as xr
 
+#############Load the dataset######################
 ds=xr.open_dataset('/mnt/e/Python_Scripts/Sample_Data/ERA5_Temperature_2020.grib',engine='cfgrib',backend_kwargs={'indexpath':''})
 
 print(ds)		###Print summary of the file
