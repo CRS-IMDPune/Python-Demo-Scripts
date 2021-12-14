a2a7c2dfa563a425b5099ebbf3d60d9149398916
Lekshmi S <94049317+lekshmis9403@users.noreply.github.com>
2021-12-14 19:57:05 +0530

    Update read_multiple.py

--- Read/grib/read_multiple.py
+++ Read/grib/read_multiple.py
@@ -1,5 +1,10 @@
+#######PYTHON CODE FOR IMD PREPARED BY LEKSHMI S#########
+########## https://doi.org/10.5281/zenodo.5674826 ############
+
+################Import necessary modules##################
 import xarray as xr
 
+############Load the dataset#############################
 ds=xr.open_mfdataset('/mnt/e/Python_Scripts/Sample_Data/ERA5_Temperature_*.grib',engine='cfgrib',backend_kwargs={'indexpath':''})
 print(ds)		###Print summary of the file
 #print(ds.t)		###Print summary of the variable
