82db1252998a1caa6b5c6128b855e87b42bf33d6
Lekshmi S <94049317+lekshmis9403@users.noreply.github.com>
2022-06-29 17:12:05 +0530

    Add files via upload

---
+++ longitude_range_conversion/longitude_conversion.py
@@ -0,0 +1,23 @@
+#####################Conversion of longitude values from 0 - 360 range to -180 -180 range and vice versa##########################
+import xarray as xr
+import numpy as np
+
+#######################Defining a function for conversion of longitudes to 0-360 range given a longitude######################
+def lon_range(lon):
+    if lon < 0:
+        lon = lon+360
+        return (lon)
+    else:
+        return (lon)
+
+test_lon=-90.0
+print('After conversion '+str(test_lon)+'='+str(lon_range(test_lon)))
+
+####################################Converting a dataset to  -180 to 180 longitude range#####################################
+data=xr.open_dataset('S:/NOAA-COBE-SST/sst.mon.mean.nc')
+print(data)
+
+data.coords['lon'] = (data.coords['lon'] + 180) % 360 - 180
+data = data.sortby(data.lon)
+print('After conversion of whole data')
+print(data)
